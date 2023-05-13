f10e7895eedeae5009ae2d179c3618a29befade0
ali-ballout <82840470+ali-ballout@users.noreply.github.com>
2023-05-14 00:44:04 +0200

    Update EntailmentCheck.java

--- owl-entailment/src/main/java/fr/uca/owl/entailment/EntailmentCheck.java
+++ owl-entailment/src/main/java/fr/uca/owl/entailment/EntailmentCheck.java
@@ -8,10 +8,6 @@ import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.*;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 
-/**
- *
- * @author Iliana Petrova
- */
 public class EntailmentCheck {
 
     public void checkEntailment(OWLOntology ontology, Set<OWLAxiom> axiomSet){
@@ -19,14 +15,14 @@ public class EntailmentCheck {
         OWLReasoner reasoner = Utils.createReasoner(ontology);
         int count = 0;
         for (OWLAxiom ax: axiomSet) {
-            if(!reasoner.isEntailed(ax)){
+            if(!reasoner.isEntailed(ax)){//remove ! if you want to see whats entailed
                 //System.out.println("Axiom: " + ax + "not entailed.");
                 System.out.println(ax);
                 count++;
             }
         }
-    //print the number of axioms that are not entailed
-    System.out.println("Number of axioms not entailed: " + count);
+    //print the number of axioms that are or are not entailed
+    System.out.println("Number: " + count);
     }
 
 
